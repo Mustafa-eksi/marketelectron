1d93a23550d87c51420ef3b52010297425bb2748
Mustafa Ekşi <mustafa.eksi@tutamail.com>
2022-12-22 00:15:04 +0300

    Added /products, /addproduct, /buyproduct (still in development) and created a database for products.

--- backend/index.js
+++ backend/index.js
@@ -33,6 +33,29 @@ function Authenticate(username, password) {
     })
 }
 
+function IsAdmin(username) {
+    return new Promise((res, rej)=>{
+        db.all("select isAdmin from accounts where username=?", [username], (err, rows) => {
+            if(err) {
+                rej(err.message);
+            }else {
+                res(rows[0]["isAdmin"] === "yes")
+            }
+        })
+    })
+}
+
+function GetProduct(name) {
+    return new Promise((res, rej)=>{
+        db.all("select * from products where name=?", [name], (err, rows)=>{
+            if(err) rej(err);
+            else {
+                res(rows[0]);
+            }
+        })
+    })
+}
+
 app.get('/', (req, res)=>{ // Connection test
     res.send("OK")
 })
@@ -92,6 +115,103 @@ app.post('/addcash', (req, res)=> {
                 res.send("Authentication is unsuccessful!")
             }
         }).catch((err)=>console.error(err))
+    }
+})
+
+app.post('/addproduct', (req, res)=>{ // req.body = {username, password, name (product name), stock, price}
+    if(!req.body.username || !req.body.password || !req.body.name || !req.body.stock || !req.body.price){
+        res.status(400);
+        res.send("Username, password and amount must be provided!");
+    }else {
+        Authenticate(req.body.username, req.body.password).then((v)=> {
+            if(v) {
+                IsAdmin(req.body.username).then((v)=>{
+                    if(v) {
+                        db.run("insert into products (name,stock,price) values (?,?,?)", [req.body.name, req.body.stock, req.body.price], (err)=>{
+                            if(err){
+                                res.status(500);
+                                res.send("Can not create product!");
+                                return console.error(err.message);
+                            }
+                        })
+                        res.status(200)
+                        res.send("Product successfully created!");
+                    }else {
+                        res.status(400)
+                        res.send("Your account is not admin!")
+                    }
+                }).catch((err)=>console.error(err));
+            }else {
+                res.status(400)
+                res.send("Authentication is unsuccessful!")
+            }
+        }).catch((err)=>console.error(err));
+    }
+})
+
+app.get('/products', (req, res)=>{
+    db.all("select name, stock, price from products", [], (err, rows)=>{
+        if(err) {
+            res.status(500);
+            res.send("Can not list products!");
+            return console.error(err.message);
+        }else {
+            res.status(200);
+            res.send(JSON.stringify({data: rows}))
+        }
+    })
+})
+
+app.post('/buyproduct', (req, res)=>{ // req.body = {username, password, name (product name), number}
+    if(!req.body.username || !req.body.password || !req.body.name || !req.body.number){
+        res.status(400);
+        res.send("Username, password and amount must be provided!");
+    }else {
+        Authenticate(req.body.username, req.body.password).then((v)=> {
+            if(v) {
+                db.all("select cash from accounts where username=?", [req.body.username], (err,rows)=>{
+                    if(err) {
+                        res.status(500);
+                        res.send("Can not access account right now!");
+                        return console.error(err);
+                    }else {
+                        GetProduct(req.body.name).then((pro)=>{
+                            if(rows[0]["cash"] >= pro.price && pro.stock >= req.body.number) {
+                                db.run("update accounts set cash=cash-? where username=?", [pro.price, req.body.username], (err)=>{
+                                    if(err) {
+                                        res.status(500)
+                                        res.send("Can not update cash! " + err.message);
+                                    }
+                                });
+                                let prebought = pro.bought_by ? pro.bought_by + "," : "";
+                                db.run("update products set bought_by = ? and last_bought_ts = ?  and stock=stock-? where name=?", [prebought+req.body.username, Date.now(), req.body.number, req.body.username], (err)=>{
+                                    if(err) {
+                                        res.status(500)
+                                        res.send("Can not update product! " + err.message);
+                                    }
+                                });
+                                res.status(200);
+                                res.send("Successfully bought!");
+                            }else {
+                                res.status(200)
+                                res.send("insufficient balance")
+                            }
+                        }).catch((err)=>{
+                            res.status(500)
+                            res.send("Can not get product! " + err)
+                            return console.error(err);
+                        })
+                    }
+                })
+            }else {
+                res.status(400)
+                res.send("Authentication is unsuccessful!")
+            }
+        }).catch((err)=>{
+            res.status(500);
+            res.send("Can not authenticate: " + err);
+            return console.error(err);
+        });
     }
 })
 
